4b1414782e1a3af18226b5ce8a6183f9a8b6cd73
dev.santhoz001@gmail.com <dev.santhoz001@gmail.com>
2024-01-23 01:32:55 +0530

    added student register entity

--- HandlingDb/Contexts/TeamDbContext.cs
+++ HandlingDb/Contexts/TeamDbContext.cs
@@ -1,6 +1,26 @@
-﻿namespace HandlingDb.Contexts
+﻿using HandlingDb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HandlingDb.Contexts
 {
-    public class TeamDbContext
+    public class TeamDbContext : DbContext
     {
+        public DbSet<StudentRegister> data { get; set; }
+        public TeamDbContext()
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configurationInstance = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory)?.FullName ?? ".")
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile("appsettings.local.json", optional: true)
+                .Build();
+
+            string dbConnString = configurationInstance["ConnectionStrings:DbStudent"] ?? "";
+            optionsBuilder.UseNpgsql(dbConnString);
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
