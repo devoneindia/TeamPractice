eee445ffef071a2ad1305fb8cb60f3f5c35250ac
devpraveen002 <dev.praveen002@gmail.com>
2024-01-24 01:26:37 +0530

    changes in cricketer_details_model

---
+++ HandlingDb/Migrations/20240123111141_AddNewModels.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HandlingDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cricketer_details",
+                columns: table => new
+                {
+                    player_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    player_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    player_number = table.Column<string>(type: "character varying(4)", maxLength: 4, nullable: false),
+                    gender = table.Column<bool>(type: "boolean", nullable: false),
+                    age = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false),
+                    city = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    nation = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    mobile = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    address = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    address2 = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    address3 = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    address4 = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cricketer_details", x => x.player_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "student_register",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    father_name = table.Column<string>(type: "text", nullable: false),
+                    dob = table.Column<DateOnly>(type: "date", nullable: false),
+                    @class = table.Column<string>(name: "class", type: "text", nullable: false),
+                    section = table.Column<string>(type: "text", nullable: false),
+                    blood_group = table.Column<string>(type: "text", nullable: false),
+                    address = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_student_register", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cricketer_details");
+
+            migrationBuilder.DropTable(
+                name: "student_register");
+        }
+    }
+}
