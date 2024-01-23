ad150954da5e78365e10e24d1063d5aab2183c16
dev.santhoz001@gmail.com <dev.santhoz001@gmail.com>
2024-01-23 17:22:18 +0530

    Added Entity for Bike

---
+++ HandlingDb/Migrations/20240123114734_BikeEntity.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HandlingDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class BikeEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ornamental_fish",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    life_span = table.Column<string>(type: "text", nullable: false),
+                    water = table.Column<string>(type: "text", nullable: false),
+                    colour = table.Column<string>(type: "text", nullable: false),
+                    origin = table.Column<string>(type: "text", nullable: false),
+                    food = table.Column<string>(type: "text", nullable: false),
+                    price_per_piece = table.Column<string>(type: "text", nullable: false),
+                    price_per_pair = table.Column<string>(type: "text", nullable: false),
+                    packing_fee = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ornamental_fish", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ornamental_fish");
+        }
+    }
+}
---
+++ HandlingDb/Models/Bike.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HandlingDb.Models
+{
+    [Table("bike")]
+    public class Bike
+    {
+        [Column]
+        [Key]
+        public int Id { get; set; }
+        [Column("brand")]
+        public string Brand { get; set; } = string.Empty;
+        [Column("model")]
+        public string Model { get; set; } = string.Empty;
+        [Column("manufacture_date")]
+        public DateOnly ManufactureDate { get; set; }
+        [Column("registration_date")]
+        public DateOnly RegistrationDate { get; set; }
+        [Column("cubic_cylinder")]
+        public string CubicCylinder { get; set; } = string.Empty;
+        [Column("chassis_number")]
+        public string ChassisNumber { get; set; } = string.Empty;
+        [Column("engine_number")]
+        public string EngineNumber { get; set; } = string.Empty;
+        [Column("wheel_size")]
+        public int WheelSize { get; set; } 
+        [Column("seat_capacity")]
+        public string SeatCapacity { get; set; } = string.Empty;
+
+    }
+}
