b171082b01c1caa9d99d76f706dfc43afbff8889
dev.santhoz001@gmail.com <dev.santhoz001@gmail.com>
2024-01-23 16:55:18 +0530

    Added new entity OrnamentalFish

--- HandlingDb/Contexts/TeamDbContext.cs
+++ HandlingDb/Contexts/TeamDbContext.cs
@@ -7,9 +7,10 @@ namespace HandlingDb.Contexts
     {
        
         public DbSet<Customer> CustomerRecords { get; set; }
-       
+        public DbSet<OrnamentalFish> fish {  get; set; }
         public DbSet<StudentRegister> data { get; set; }
         public DbSet<CricketerDetails> CricketerDeatils { get; set; }
+        public DbSet<Car> cars { get; set; }
         public TeamDbContext()
         {
             AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
@@ -21,7 +22,7 @@ namespace HandlingDb.Contexts
                 .AddJsonFile("appsettings.json", optional: true)
                 .AddJsonFile("appsettings.local.json", optional: true)
                 .Build();
-            string dbConnString = configurationInstance["ConnectionStrings:DbStudent"] ?? "";
+            string dbConnString = configurationInstance["ConnectionStrings:Db"] ?? "";
             optionsBuilder.UseNpgsql(dbConnString);
             base.OnConfiguring(optionsBuilder);
         }
