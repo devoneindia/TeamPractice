911446435650cb0cc037f007c0d57e983c51977a
dev.santhoz001@gmail.com <dev.santhoz001@gmail.com>
2024-01-23 18:37:20 +0530

    Added Desktop Entity

---
+++ HandlingDb/Models/Desktop.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HandlingDb.Models
+{
+    [Table("desktop")]
+    public class Desktop
+    {
+        [Column("id")]
+        [Key]
+        public int Id { get; set; }
+        [Column("brand")]
+        public string Brand { get; set; } = string.Empty;
+        [Column("processor")]
+        public string Processor { get; set; } = string.Empty;
+        [Column("ram")]
+        public string Ram { get; set; } = string.Empty;
+        [Column("rom")]
+        public string Rom { get; set; } = string.Empty;
+        [Column("graphics")]
+        public string Graphics { get; set; } = string.Empty;
+        [Column("ssd")]
+        public string Ssd { get; set; } = string.Empty;
+        [Column("os")]
+        public string Os { get; set; } = string.Empty;
+        [Column("os_version")]
+        public string OsVersion { get; set; } = string.Empty;
+        [Column("price")]
+        public int Price { get; set; }
+        [Column("manufacture_date")]
+        public DateOnly ManufactureDate { get; set; }
+    }
+    
+}
