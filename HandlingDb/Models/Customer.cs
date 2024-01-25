5c7bd48182844b147e73e9b5195c20bdf2d42f39
Akila Peramatchi <dev.akila001@outlook.com>
2024-01-25 21:37:53 +0530

    Added Customer Pages

--- HandlingDb/Models/Customer.cs
+++ HandlingDb/Models/Customer.cs
@@ -21,31 +21,31 @@ namespace HandlingDb.Models
         public string Email { get; set; } = string.Empty;
         [MaxLength(500)]
         [Column("phone")]
-        public string Phone { get; set; }
+        public string Phone { get; set; } = string.Empty.ToString();
         [MaxLength(500)]
         [Column("addressLine1")]
-        public string AddressLine1 { get; set; }
+        public string AddressLine1 { get; set; } = string.Empty;
         [MaxLength(500)]
         [Column("addressLine2")]
-        public string AddressLine2 { get; set; }
+        public string AddressLine2 { get; set; } = string.Empty;
         [MaxLength(500)]
         [Column("addressLine3")]
-        public string AddressLine3 { get; set; }
+        public string AddressLine3 { get; set; } = string.Empty;
         [MaxLength(500)]
         [Column("addressLine4")]
-        public string AddressLine4 { get; set; }
+        public string AddressLine4 { get; set; } = string.Empty;
         [MaxLength(500)]
         [Column("state")]
-        public string State { get; set; }
+        public string State { get; set; } = string.Empty;
         [MaxLength(500)]
         [Column("pinCode")]
-        public string PinCode { get; set; }
+        public string PinCode { get; set; } = string.Empty;
         [MaxLength(500)]
         [Column("dateofbirth")]
-        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; } = DateTime.Now;
         [MaxLength(500)]
         [Column("subscriptionstatus")]
-        public bool SubscriptionStatus { get; set; }
+        public bool SubscriptionStatus { get; set; } = false;
 
         public virtual Product Products { get; set; }
 
