d01f684830121d4c0da656f4951dea5f14c0a057
Akila Peramatchi <dev.akila001@outlook.com>
2024-01-25 23:23:07 +0530

    Created new OnlineProject

---
+++ OnlineShop/Migrations/20240125174837_Updated_Again.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace OnlineShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updated_Again : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "aki_order",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    product_id = table.Column<int>(type: "integer", nullable: false),
+                    customer_id = table.Column<int>(type: "integer", nullable: false),
+                    order_date = table.Column<DateTime>(type: "timestamp without time zone", maxLength: 100, nullable: false),
+                    ProductsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_aki_order", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_aki_order_aki_customer_customer_id",
+                        column: x => x.customer_id,
+                        principalTable: "aki_customer",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_aki_order_aki_product_ProductsId",
+                        column: x => x.ProductsId,
+                        principalTable: "aki_product",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_aki_order_customer_id",
+                table: "aki_order",
+                column: "customer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_aki_order_ProductsId",
+                table: "aki_order",
+                column: "ProductsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "aki_order");
+        }
+    }
+}
